81e97cf8a99035d87ccfdea915e64a81b0eac8f2
Penaz <Penaz91@users.noreply.github.com>
2017-09-10 18:37:41 +0200

    Create cbr2cbz.sh
    Added Cbr2Cbz

---
+++ cbr2cbz.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cbr=$1
+start_directory=`pwd`
+
+cbz=`basename "$cbr" .cbr`.cbz
+
+tempdir=`mktemp -d`
+cd "$tempdir"
+unrar e -inul "$start_directory/$cbr"
+zip -q "$start_directory/$cbz" *
+rm -rf "$tempdir"
